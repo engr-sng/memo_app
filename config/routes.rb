33da761d60681fa5c8eabd4a16c523092135bd22
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2022-11-26 11:26:55 +0900

    [add]CRUD機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'memos#index'
+  resources :memos, only: [:index, :update, :destroy, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
